7224b7267f7070cf7290a12108938ab44f7dc179
Maryus Martsyalis <martsyalis@gmail.com>
2020-08-06 02:19:32 -0700

    list-comprehension

--- list-comprehension.py
+++ list-comprehension.py
@@ -6,14 +6,9 @@ data = {
 def greeting(name, age):
     return 'Hi, My name is {} and Im {} years old'.format(name, age)
 
-print(greeting(**data))
-
 def foo(**kwargs):
   for key, value in kwargs.items():
     print("{}:{}".format(key, value))
-
-
-foo(name = "Maryus", age = 29)
 
 def add(*numbers):
     total = 0
@@ -23,4 +18,14 @@ def add(*numbers):
 
 total = add(1,2,3,4)
 
-print('total is: ', total)
+
+def solve(s):
+    indexes = [i for i, c in enumerate(s) if c == " "]
+    result = "".join([c for c in s if c!=" "])[::-1]
+
+    for index in indexes:
+      result = result[:index] + " " + result[index:]
+      
+    return result
+
+print(solve("our code"))
